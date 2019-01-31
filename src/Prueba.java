7b64f1c5d539d57eda354f17dbac8ba806946062
Cristofer Casas <cristofer32513@gmail.com>
2019-01-31 11:40:27 -0600

    Se agrego el cotructor de la VentanaPrincipal y se configuro, ademas se agrego la primera etiqueta y se creo la llamada en el main.

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,14 +2,48 @@ import javax.swing.*;
 import java.awt.*;
 
 class VentanaPrincipal extends JFrame {
+	public VentanaPrincipal(){
+		//getContentPane().setLayout(new FlowLayout());//Layout del JFrame
+		getContentPane().setLayout(null);
+		setSize(1355, 756);
+		setTitle("Formulario de Registro");
+		setLocationRelativeTo(null);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		
+		JLabel label1=new JLabel("<html> <body>The <b>Classic Form</b> Includes all visible fields for this list. </body></html>");
+		label1.setBounds(10, 10, 360, 40);
+		label1.setFont(new Font("Arial", 5, 18));
+		add(label1);
 	
+	
+	
+	}
 }
 
 public class Prueba {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		try {
+			UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
+				
+		SwingUtilities.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				new VentanaPrincipal();
+			}
+		});
+		
+		System.gc();
 	}
-
 }
