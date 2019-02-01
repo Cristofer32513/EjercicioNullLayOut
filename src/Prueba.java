63da5175698e65d55e3c863c0190fd51455c98ef
Cristofer Casas <cristofer32513@gmail.com>
2019-02-01 12:25:58 -0600

    Se realiazaron diversas modificaciones como posicion de elementos y agregacion de botones, cajas de texto, etiquetas, etc.

--- src/Prueba.java
+++ src/Prueba.java
@@ -108,29 +108,78 @@ class VentanaPrincipal extends JFrame {
 		add(label7);
 		
 		JPanel panel1=new JPanel(null);
-			panel1.setBorder(BorderFactory.createTitledBorder(""));
-			panel1.setBounds(380, 40, 600, 260);
-			
+		panel1.setBounds(380, 40, 610, 260);
+		panel1.setBorder(BorderFactory.createTitledBorder(""));
+		
+		JLabel label8=new JLabel("<html><body><font color=red>* </font>Inidcates required</body></html>");
+		label8.setBounds(470, -5, 260, 30);
+		label8.setFont(new Font("Times New Roman", 5	, 14));
+		panel1.add(label8);
+		
+		JLabel label9=new JLabel("Email address");
+		label9.setBounds(10, 10, 160, 30);
+		label9.setFont(new Font("Times New Roman", 5, 18));
+		panel1.add(label9);
+		
+		JTextField txtCaja3=new JTextField("*");
+		txtCaja3.setHorizontalAlignment(JTextField.RIGHT);
+		txtCaja3.setForeground(Color.RED);
+		txtCaja3.setBounds(10, 35, 590, 35);
+		txtCaja3.setFont(new Font("Times New Roman", 5, 18));
+		txtCaja3.setBackground(new Color(240, 240, 240));
+		panel1.add(txtCaja3);
+		
+		JLabel label10=new JLabel("First name");
+		label10.setBounds(10, 75, 160, 30);
+		label10.setFont(new Font("Times New Roman", 5, 18));
+		panel1.add(label10);
+		
+		JTextField txtCaja4=new JTextField();
+		txtCaja4.setBounds(10, 100, 590, 35);
+		txtCaja4.setFont(new Font("Times New Roman", 5, 18));
+		txtCaja4.setBackground(new Color(240, 240, 240));
+		panel1.add(txtCaja4);
+		
+		JLabel label11=new JLabel("Last name");
+		label11.setBounds(10, 140, 160, 30);
+		label11.setFont(new Font("Times New Roman", 5, 18));
+		panel1.add(label11);
+		
+		JTextField txtCaja5=new JTextField();
+		txtCaja5.setBounds(10, 165, 590, 35);
+		txtCaja5.setFont(new Font("Times New Roman", 5, 18));
+		txtCaja5.setBackground(new Color(240, 240, 240));
+		panel1.add(txtCaja5);
+		
+		ImageIcon imagen=new ImageIcon("./src/images.jpeg");
+		ImageIcon imagenRed=new ImageIcon(imagen.getImage().getScaledInstance(160, 60, Image.SCALE_REPLICATE));
+				
+		JButton btn1=new JButton(imagenRed);
+		btn1.setBounds(10, 205, 130, 40);
+		panel1.add(btn1);
+		
 		add(panel1);
 		
-		JLabel label8=new JLabel("Copy/past Onto your site");
-		label8.setBounds(380, 320, 300, 30);
-		label8.setFont(new Font("Arial", 5	, 24));
-		add(label8);
 		
-		JTextArea txtCaja3=new JTextArea();
-		txtCaja3.setText("<html>\n\n<head>\n<title>El primer documento HTML</title>\n</head>"
+		JLabel label12 = new JLabel("Copy/past Onto your site");
+		label12.setBounds(380, 320, 300, 30);
+		label12.setFont(new Font("Arial", 5	, 24));
+		add(label12);
+		
+		JTextArea txtCaja7=new JTextArea();
+		txtCaja7.setText("<html>\n\n<head>\n<title>El primer documento HTML</title>\n</head>"
 				+ "\n\n<body>\n<p>El lenguaje HTML es <strong>tan sencillo</strong> que"
 				+ "prácticamente se entiende sin estudiar el significadode sus etiquetas"
 				+ "principales.</p>\n</body>\n\n</html>");
-		txtCaja3.setBounds(380, 360, 600, 250);
-		txtCaja3.setFont(new Font("Times New Roman", 5, 16));
-		txtCaja3.setBackground(new Color(230, 230, 230));
-		txtCaja3.setEditable(false);
-		txtCaja3.setBorder(BorderFactory.createTitledBorder(""));
-		txtCaja3.setLineWrap(true);
-		txtCaja3.setWrapStyleWord(true);
-		add(txtCaja3);
+		txtCaja7.setFont(new Font("Times New Roman", 5, 16));
+		txtCaja7.setBackground(new Color(230, 230, 230));
+		txtCaja7.setEditable(false);
+		txtCaja7.setLineWrap(true);
+		txtCaja7.setWrapStyleWord(true);
+		JScrollPane scrollPane2 = new JScrollPane(txtCaja7);
+		scrollPane2.setBounds(380, 360, 610, 210);
+		scrollPane2.setBorder(BorderFactory.createTitledBorder(""));
+		add(scrollPane2);
 		
 	}
 }
